02d63ada6eb097d6efdcb3901c5260a5091155bf
Alexander Liao <alexander_liao@outlook.com>
2018-07-07 22:23:20 -0400

    upload model

---
+++ build/dummy-07/utility/nimfa/__init__.py
@@ -0,0 +1,28 @@
+
+"""
+#################
+Nimfa (``nimfa``)
+#################
+
+Nimfa is a Python module which includes a number of published matrix
+factorization algorithms, initialization methods, quality and performance
+measures and facilitates the combination of these to produce new strategies.
+The library represents a unified and efficient interface to matrix
+factorization algorithms and methods.
+"""
+
+__license__ = 'BSD'
+__version__ = '1.3.4'
+__maintainer__ = 'Marinka Zitnik'
+__email__ = 'marinka@cs.stanford.edu'
+
+
+import os, sys
+sys.path.append(
+    os.path.join( os.path.dirname( __file__ ), '..' )
+)
+
+from nimfa import examples
+from nimfa.methods.factorization import *
+# from .version import \
+#     short_version as __version__, git_revision as __git_version__
